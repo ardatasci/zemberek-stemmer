4541574d890fb26a9817cacaa0c9cd609a62e86b
YunusEmre <yemre083@gmail.com>
2014-08-09 14:35:50 +0300

    word type changed

--- ContentAugmentation/src/KeywordExtraction/NamedEntityTurkish/Word.java
+++ ContentAugmentation/src/KeywordExtraction/NamedEntityTurkish/Word.java
@@ -11,7 +11,7 @@ public class Word {
 	
 	String affix = "";
 	//String type = null;
-	WordType type = WordType.NOTHING;
+	WordType type = null;
 	WordType subType = null;
 	private String wikiText = "";
 	
